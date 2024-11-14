147b36e4d95a018568de1eb80d1c2ea9bc2e32f6
Rubel <work.mdrubelahamed@gmail.com>
2024-11-14 10:18:59 +0530

    full DOM task is done

--- js/dom/dom-task.js
+++ js/dom/dom-task.js
@@ -31,10 +31,51 @@ document.getElementById("addNewItem").addEventListener("click", function () {
 })
 
 // example 5
-document.getElementById("removeLastTask").addEventListener("click", function() {
+document.getElementById("removeLastTask").addEventListener("click", function () {
     // let lastTask = document.getElementById("taskList").lastElementChild
     // lastTask.remove()
 
     let lastTask = document.querySelector("#taskList li:last-child")
     lastTask.remove() // both way the code will work the same
+})
+
+// example 6
+document.getElementById("clickMeButton").addEventListener("click", function () {
+    alert("I got clicked!")
+})
+
+// example 7
+document.getElementById("teaList").addEventListener("click", (event) => {
+    // if (event.target && event.target.classList.contains("teaItem")) {
+    //     alert(`You selected: ${event.target.textContent}`)
+    // }
+
+    if ((event.target.li = "teaItem")) {
+        alert(`You selected: ${event.target.textContent}`)
+    }
+})
+
+// example 8
+document.getElementById("feedbackForm").addEventListener("submit", function (event) {
+    event.preventDefault()
+
+    let feedbackDisplay = document.querySelector("#feedbackDisplay")
+    feedbackDisplay.textContent = "" // textcontent make it empty
+    let feedback = document.querySelector("#feedbackInput").value // grab the feedback
+    let lableContent = document.querySelector("label[for='feedbackInput']").textContent // grab the label textcontent
+    feedbackDisplay.textContent = `${lableContent} ${feedback}` // added the feedback
+
+    // add css
+    feedbackDisplay.classList.add("highlight")
+    feedbackDisplay.style.fontSize = "1.5rem"
+})
+
+// exmaple 9
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("#domStatus").textContent = "dom fully loaded"
+})
+
+// example 10
+document.querySelector("#toggleHighlight").addEventListener("click", function(){
+    document.querySelector("#colorChnagingText").classList.toggle("highlight")
 })
